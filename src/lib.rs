71244512f366bbd159d8257273f0d785859066de
Christian Lewe <ch_lewe@agdsn.me>
2024-07-04 14:59:10 +0200

    Rust: Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+//! Interface between Simfony as Rust
+//!
+//! The [Simfony](https://github.com/BlockstreamResearch/simfony) language feels very similar to Rust.
+//! This crate provides tools to work with Simfony as literal Rust.
